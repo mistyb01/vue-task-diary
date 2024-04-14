d959908bd8b28eaa00427b412a97610a46a88042
Misty <mistyb140@gmail.com>
2024-04-14 13:38:51 -0700

    hover effect and transition

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -6,12 +6,16 @@ defineProps(["title"]);
 </script>
 
 <template>
-  <div class="flex justify-between p-4">
+  <div
+    class="group flex justify-between p-4 rounded hover:bg-gray-100 transition-colors duration-150"
+  >
     <div class="flex gap-4">
       <input v-model="model" type="checkbox" />
       <span>{{ title }}</span>
     </div>
-    <div class="flex gap-4">
+    <div
+      class="opacity-0 group-hover:opacity-100 transition-opacity duration-150 flex gap-4"
+    >
       <PencilIcon class="h-6 w-6" />
       <TrashIcon class="h-6 w-6" />
     </div>
