8d97b9a8dfea8851ceb5ecfe0f80e86adfaa79cd
Misty <mistyb140@gmail.com>
2024-04-14 13:18:54 -0700

    use edit and del icons

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,19 @@
 <script setup>
+import { TrashIcon, PencilIcon } from "@heroicons/vue/24/outline";
+
 const model = defineModel();
 defineProps(["title"]);
 </script>
 
 <template>
-  <div class="flex gap-2 p-4">
-    <input v-model="model" type="checkbox" />
-    <span>{{ title }}</span>
+  <div class="flex justify-between p-4">
+    <div class="flex gap-4">
+      <input v-model="model" type="checkbox" />
+      <span>{{ title }}</span>
+    </div>
+    <div class="flex gap-4">
+      <PencilIcon class="h-6 w-6" />
+      <TrashIcon class="h-6 w-6" />
+    </div>
   </div>
 </template>
