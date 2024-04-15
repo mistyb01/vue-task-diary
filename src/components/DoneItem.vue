f96ef287e9acff78a4d57e6dbd8f8a58dd9dad7b
Misty <mistyb140@gmail.com>
2024-04-14 17:04:37 -0700

    undo check on hover

--- src/components/DoneItem.vue
+++ src/components/DoneItem.vue
@@ -1,10 +1,16 @@
 <script setup>
+import { ArrowUturnLeftIcon } from "@heroicons/vue/24/outline";
 defineProps(["title"]);
 </script>
 
 <template>
   <div class="flex gap-2 p-4">
-    <span>&#x2713;</span>
+    <button class="group">
+      <span class="group-hover:hidden">&#x2713;</span>
+      <span class="group-hover:block hidden">
+        <ArrowUturnLeftIcon class="w-4 h-4" />
+      </span>
+    </button>
     <span>{{ title }}</span>
   </div>
 </template>
