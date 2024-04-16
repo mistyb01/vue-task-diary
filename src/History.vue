5a954d3cf37a61c291ead1eb06c4463e2052933e
Misty <mistyb140@gmail.com>
2024-04-16 11:17:13 -0700

    simple routing between todo page and history page

---
+++ src/History.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, computed } from "vue";
+</script>
+
+<template>
+  <div class="container mx-auto p-4 max-w-screen-sm min-h-screen">
+    <header class="my-8">
+      <h1 class="font-semibold text-2xl text-pink-500">History</h1>
+    </header>
+
+    <main>
+      <a href="#/">go back</a>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+</style>
