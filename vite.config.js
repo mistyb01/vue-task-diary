61f23d8b4c3a371412860ecb4c02ad339a948bef
Misty <mistyb140@gmail.com>
2024-04-23 12:09:48 -0700

    change base url in config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "https://mistyb01.github.io/vue-task-diary/",
+  base: "https://vue-task-diary.netlify.app/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
