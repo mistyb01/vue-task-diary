34216fba0e8f9e8be1ffbf6d6ecf6af5d8fa5c8d
Misty <mistyb140@gmail.com>
2024-04-26 15:35:45 -0700

    task div styling

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -31,53 +31,57 @@ const iconStyles = "h-5 w-5 hover:text-pink-500";
 </script>
 
 <template>
-  <div
-    class="flex justify-between p-4 rounded hover:bg-pink-50 transition-colors duration-150"
-  >
-    <div class="flex gap-4">
-      <input @click="$emit('checkTodo', id)" type="checkbox" />
-      <div>
-        <span v-if="!isEditing">{{ title }}</span>
-        <input
-          v-else
-          v-model="editedTitle"
-          type="text"
-          class="bg-transparent border-dotted border-2 border-gray-400"
-        />
+  <div class="rounded hover:bg-pink-50 transition-colors duration-150">
+    <div
+      class="flex justify-between p-4"
+    >
+      <div class="flex gap-4">
+        <input @click="$emit('checkTodo', id)" type="checkbox" />
+        <div>
+          <span v-if="!isEditing">{{ title }}</span>
+          <input
+            v-else
+            v-model="editedTitle"
+            type="text"
+            class="bg-transparent border-dotted border-2 border-gray-400"
+          />
+        </div>
+      </div>
+      <div class="flex gap-4">
+        <button
+          v-if="isEditing"
+          @click="
+            () => {
+              $emit('submitEdit', id, editedTitle);
+              isEditing = false;
+            }
+          "
+        >
+          <CheckIcon :class="iconStyles" />
+        </button>
+        <button v-if="!isEditing" @click="() => toggleSubtaskInput()">
+          <SubtaskIcon />
+        </button>
+        <button @click="() => toggleEditMode(title)">
+          <PencilIcon v-if="!isEditing" :class="iconStyles" />
+          <XMarkIcon v-else :class="iconStyles" />
+        </button>
+        <button v-if="!isEditing" @click="$emit('deleteTodo', id)">
+          <TrashIcon :class="iconStyles" />
+        </button>
       </div>
     </div>
-    <div class="flex gap-4">
-      <button
-        v-if="isEditing"
-        @click="
-          () => {
-            $emit('submitEdit', id, editedTitle);
-            isEditing = false;
-          }
-        "
-      >
-        <CheckIcon :class="iconStyles" />
-      </button>
-      <button v-if="!isEditing" @click="() => toggleSubtaskInput()">
-        <SubtaskIcon />
-      </button>
-      <button @click="() => toggleEditMode(title)">
-        <PencilIcon v-if="!isEditing" :class="iconStyles" />
-        <XMarkIcon v-else :class="iconStyles" />
-      </button>
-      <button v-if="!isEditing" @click="$emit('deleteTodo', id)">
-        <TrashIcon :class="iconStyles" />
-      </button>
+    <div v-if="isAddingSubtask">
+      <SubtaskInput @addSubtask="(title)=>emit('addSubtask', id, title)"/>
     </div>
-  </div>
-  <div v-if="isAddingSubtask">
-    <SubtaskInput @addSubtask="(title)=>emit('addSubtask', id, title)"/>
-  </div>
-  <div v-if="subtasks.length > 0">
-    <SubtaskItem
-      v-for="subtask in subtasks"
-      :key="subtask.id"
-      :title="subtask.title"
-    />
+    <div 
+      v-if="subtasks.length > 0"
+      class="pl-8">
+      <SubtaskItem
+        v-for="subtask in subtasks"
+        :key="subtask.id"
+        :title="subtask.title"
+      />
+    </div>
   </div>
 </template>
