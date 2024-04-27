8ae7ebba68d8b04b305e65923402a9bdf433e7a8
Misty <mistyb140@gmail.com>
2024-04-26 19:24:13 -0700

    spacing edits

--- src/components/SubtaskItem.vue
+++ src/components/SubtaskItem.vue
@@ -5,7 +5,7 @@ defineProps(["title"]);
 
 <template>
   <div
-    class="flex justify-between p-4 rounded hover:bg-pink-50 transition-colors duration-150"
+    class="flex justify-between p-2 rounded hover:bg-pink-50 transition-colors duration-150"
   >
     <div class="flex gap-4">
       <input v-model="model" type="checkbox" />
