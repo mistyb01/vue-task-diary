06af97aa62e742b89dbbfe12abeb7883d74626af
Misty <mistyb140@gmail.com>
2024-04-27 10:05:50 -0700

    done item spacing

--- src/components/DoneItem.vue
+++ src/components/DoneItem.vue
@@ -6,7 +6,7 @@ const emit = defineEmits(["undoComplete"]);
 
 <template>
   <div class="flex flex-col">
-    <div class="flex gap-2 items-center p-2">
+    <div class="flex gap-2 items-center pt-2">
       <button @click="$emit('undoComplete', id)" class="group">
         <span class="group-hover:hidden"><CheckIcon class="w-4 h-4" /></span>
         <span class="group-hover:block hidden">
@@ -16,7 +16,7 @@ const emit = defineEmits(["undoComplete"]);
       <span>{{ title }}</span>
     </div>
     <ul v-if="subtasks.length > 0" class="pl-8">
-        <li v-for="item in subtasks" class="flex gap-2 items-center p-2">
+        <li v-for="item in subtasks" class="flex gap-2 items-center">
           <span v-if="item.completed"><CheckIcon class="w-3 h-3" /></span>
           <span v-else class="border border-gray-400 w-3 h-3"></span>
           {{ item.title }}
