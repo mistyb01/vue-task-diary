fe21c6cbe1f45f4ada6a2da8a73c0baa1f477d69
Misty <mistyb140@gmail.com>
2024-04-27 09:22:59 -0700

    page title, remove image

--- src/components/EmptyMessage.vue
+++ src/components/EmptyMessage.vue
@@ -4,7 +4,6 @@ defineProps(["msg"]);
 
 <template>
   <div class="flex flex-col items-center text-gray-500 gap-4 py-8">
-    <img src="@/assets/cat_study.png" width="200"/>
     <span>{{ msg }}</span>
   </div>
 </template>
