704f1081b603b2c6896921b408ca512895de7628
Misty <60805050+mistyb01@users.noreply.github.com>
2024-04-30 14:07:09 -0700

    history page: visual improvements (#14)
    * filter completed items to today
    
    * done item spacing
    
    * add hr between dates
    
    * subtasks are toggleable

--- src/components/DoneItem.vue
+++ src/components/DoneItem.vue
@@ -1,25 +1,44 @@
 <script setup>
-import { ArrowUturnLeftIcon, CheckIcon } from "@heroicons/vue/24/outline";
+import { ref } from 'vue';
+import { ArrowUturnLeftIcon, CheckIcon, ChevronRightIcon, ChevronUpIcon } from "@heroicons/vue/24/outline";
+
 defineProps(["title", "id", "subtasks"]);
 const emit = defineEmits(["undoComplete"]);
+const showSubtasks = ref(false);
+
 </script>
 
 <template>
   <div class="flex flex-col">
-    <div class="flex gap-2 items-center p-2">
-      <button @click="$emit('undoComplete', id)" class="group">
+    <div class="flex justify-between">
+      <div class="flex gap-2 items-center pt-2 ">
+        <button @click="$emit('undoComplete', id)" class="group">
         <span class="group-hover:hidden"><CheckIcon class="w-4 h-4" /></span>
         <span class="group-hover:block hidden">
           <ArrowUturnLeftIcon class="w-4 h-4" />
         </span>
       </button>
       <span>{{ title }}</span>
+      </div>
+      <button class="text-xs text-gray-400 rounded uppercase" v-if="subtasks.length > 0"
+        @click="()=>showSubtasks = !showSubtasks">
+      <span v-if="!showSubtasks" class="flex items-center">
+        expand
+        <ChevronRightIcon class="w-4 h-4 inline ml-1"/>
+      </span>
+      <span v-else class="flex items-center">
+        hide
+        <ChevronUpIcon class="w-4 h-4 inline ml-1"/>
+      </span>
+    </button>
     </div>
-    <ul v-if="subtasks.length > 0" class="pl-8">
-        <li v-for="item in subtasks" class="flex gap-2 items-center p-2">
+    <ul v-if="subtasks.length > 0 && showSubtasks" class="pl-8">
+        <li v-for="item in subtasks" class="flex gap-2 items-center">
           <span v-if="item.completed"><CheckIcon class="w-3 h-3" /></span>
           <span v-else class="border border-gray-400 w-3 h-3"></span>
-          {{ item.title }}
+          <span class="text-gray-400 text-sm">
+            {{ item.title }}
+          </span>
         </li>
       </ul>
   </div>
